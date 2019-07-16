ddd70d5cc7cf11bcbefb34353f677b65c7a98ad9
Ali Hussain Khan <36451371+ahk4815@users.noreply.github.com>
2019-07-16 23:35:49 +0530

    level A problem from codeforces education round 68

---
+++ remove a progression.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ 
+int main(void) {
+	ll t,n,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>x;
+	    cout<<2*x<<endl;
+	}
+	
+}
